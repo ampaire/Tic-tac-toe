98448da33830a990096ef454dcd70f6671dbdfc0
ampaire <femayapr@gmail.com>
2019-10-31 10:16:38 -0600

    Fix indentation linting errors

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,19 +5,19 @@ require_relative './players.rb'
 class GameLogic
   attr_reader :board, :player_one, :player_two, :X ,:O
     def initialize(players, board = (1..9).to_a)
-        @board = board
-        @player_one = Player.new(players)
-        @player_two = Player.new(players)
+      @board = board
+      @player_one = Player.new(players)
+      @player_two = Player.new(players)
     end
 
     SURE_WINS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6] , [1,4,7], [2,5,8], [0,4,8], [2,4,6]].freeze
 
     def won
-        SURE_WINS.detect do |combo|
-            @board [combo[0]] == @board [combo[1]] &&
-            @board [combo[1]] == @board [combo[2]] &&
-            board_full?(combo[0])
-        end
+      SURE_WINS.detect do |combo|
+        @board [combo[0]] == @board [combo[1]] &&
+        @board [combo[1]] == @board [combo[2]] &&
+          board_full?(combo[0])
+      end
     end
 
     def player_index(index, input = :X)
@@ -25,53 +25,53 @@ class GameLogic
     end
     
     def board_full?(index)
-        @board[index] == :X || @board[index] == :O
+      @board[index] == :X || @board[index] == :O
     end
 
     def take_place(inpt)
-        inpt.to_i - 1
+      inpt.to_i - 1
     end
 
     def count_turns
-        @board.select{ |e| e == :X || e == :O}.size
+      @board.select{ |e| e == :X || e == :O}.size
     end
 
 
     def valid_move?(position)
-        position.between?(0,8) && !board_full?(position)
+      position.between?(0,8) && !board_full?(position)
     end
 
     def full?
-        count_turns == 9
+      count_turns == 9
     end
     
     def draw?
-        !won && full?
+      !won && full?
     end
     
     def over?
-        won || full? || draw?
+      won || full? || draw?
     end
 
     def get_current_player
-        if (count_turns%2).zero?
-            current_player = :X
-        else
-            current_player = :O
-        end
+      if (count_turns%2).zero?
+          current_player = :X
+      else
+          current_player = :O
+      end
         current_player
     end
 
     def winner?
-        if @board[won.first] == :X
-            :X
-        else
-            :O
-        end
+      if @board[won.first] == :X
+          :X
+      else
+          :O
+      end
     end
 
     def game_over
-        won || board_full?||game_draw
+      won || board_full?||game_draw
     end
 
 end
