1aecd9e92b5416fe05856d038efcc6365b2c2395
David Elì <davidelimg1@gmail.com>
2019-11-06 17:44:53 -0600

    working on rspec testing

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game_logic.rb'
+require_relative '../lib/players.rb'
 
 describe GameLogic do
-  let(:sample_won) { [X, X, X, 4, 5, 6, 7, 8, 9] }
+  let(:board) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
 
     describe "#won" do 
       context 'if there is a win move in the board' do
         it 'returns true' do
-          its_won = sample_won.won
-          expect(its_won).to be true
+          @game = GameLogic.new(@players)
+          result = @game.won
+          expect(result).to be true
         end
       end
     end
