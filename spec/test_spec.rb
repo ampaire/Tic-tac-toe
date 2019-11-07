4b25215e567f7a6da0bb996a717367392356869b
David Elì <davidelimg1@gmail.com>
2019-11-06 23:59:48 -0600

    Tested won method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game_logic.rb'
-@players = ['Effie', 'David']
-@mock_game = GameLogic.new(@players)
-
+players = ['Effie', 'David']
+mock_game = GameLogic.new(players)
 
 describe GameLogic do
     describe "#won" do 
-      context 'if there is a win move in the board' do
-        it 'returns true' do
+      context 'Check if theres a win move and check the pattern' do
+        it 'returns the first element where is not false' do 
+          mock_game.player_index(1)
+          mock_game.player_index(4)
+          mock_game.player_index(7)
+          result = mock_game.won
+          expect(result).to eql([1, 4, 7])
         end
       end
     end
