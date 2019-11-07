bd905a868752ccad9100ed58e062659b659ab8e1
David Elì <davidelimg1@gmail.com>
2019-11-07 03:35:26 -0600

    implemented tests to full? method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -13,7 +13,7 @@ end
 describe GameLogic do
     describe "#won" do 
       context 'If theres a win pattern in the board' do
-        subject { result = mock_game.won }
+        subject { mock_game.won }
         it 'returns an array whit the winning pattern' do 
           mock_game.player_index(1)
           mock_game.player_index(4)
@@ -23,7 +23,7 @@ describe GameLogic do
       end
 
       context 'If theres no a win pattern in the board' do
-        subject { result = empty_game.won }
+        subject { empty_game.won }
         it 'returns nil' do
           expect(subject).to be nil
         end
@@ -32,7 +32,7 @@ describe GameLogic do
 
     describe '#player_index' do
       context 'If is given an index and a symbol :X or :O' do
-        subject { result = mock_game.player_index(2) }
+        subject { mock_game.player_index(2) }
         it 'Returns the symbol to be filled in the slot (index)' do
           expect(subject).to eql(:X)
         end
@@ -48,7 +48,7 @@ describe GameLogic do
       end
 
       context 'If the slot in the board is not filled' do
-        subject { result = mock_game.board_full?(5) } 
+        subject { mock_game.board_full?(5) } 
         it 'returns false' do
           expect(subject).to be false
         end
@@ -57,14 +57,14 @@ describe GameLogic do
 
     describe '#take_place' do
       context 'Recieve the position as string input' do
-        subject { result = mock_game.take_place(3) }
+        subject { mock_game.take_place(3) }
         it 'returns the input as integer' do
           expect(subject).to be_a(Integer)
         end
       end
 
       context 'Recieve a letter as input for position' do
-        subject {  result = mock_game.take_place('w') }
+        subject { mock_game.take_place('w') }
         it 'returns -1' do 
           expect(subject).to eql(-1)
         end
@@ -73,46 +73,46 @@ describe GameLogic do
 
     describe '#count_turns' do
       context 'If there are filled  slots in the board' do
-        subject { result = mock_game.count_turns }
+        subject { mock_game.count_turns }
         it 'returns the number of filled slots' do
           expect(subject).to eql(4)
         end
       end
 
       context 'If there is no filled slots in the board' do
-        it 'returns 0 slots filled' do 
-          result = empty_game.count_turns
-          expect(result).to eql(0)
+        subject { empty_game.count_turns }
+        it 'returns 0 slots filled' do
+          expect(subject).to eql(0)
         end
       end
     end
 
     describe '#available_moves' do
       context 'If there is available moves in the board' do
+        subject { mock_game.available_moves }
         it 'return an array with the available moves' do
-          result = mock_game.available_moves
-          expect(result).to eql([1, 4, 6, 7, 9])
+          expect(subject).to eql([1, 4, 6, 7, 9])
         end
       end
 
       context 'If there is no available moves in the board' do
+        subject { full_game.available_moves }
         it 'return an empty array' do
-          result = full_game.available_moves
-          expect(result).to eql([])
+          expect(subject).to eql([])
         end
       end
     end
 
     describe '#valid_move?' do
       context 'If the given input is a valid move' do
-        subject { result = mock_game.valid_move?(5) }
+        subject {  mock_game.valid_move?(5) }
         it 'returns true' do
           expect(subject).to be true
         end
       end
       
       context 'If the given input is not a valid move' do
-        subject { result = mock_game.valid_move?(2) }
+        subject {  mock_game.valid_move?(2) }
         it 'return false' do
           expect(subject).to be false
         end
@@ -121,14 +121,14 @@ describe GameLogic do
 
     describe '#valid_char?' do
       context 'If the given input is a valid selection (y/n)' do
-        subject { result = mock_game.valid_char?('Y') }
+        subject { mock_game.valid_char?('Y') }
         it 'returns true' do
           expect(subject).to be true
         end
       end
 
       context 'If the given input is a invalid selection' do
-        subject { result = mock_game.valid_char?('W') }
+        subject { mock_game.valid_char?('W') }
         it 'returns false' do
           expect(subject).to be false
         end
@@ -149,5 +149,21 @@ describe GameLogic do
           expect(subject).to be false 
         end
       end
+    end
+
+    describe '#full?' do
+      context 'If the counter of turns reach 9' do
+        subject { full_game.full? }
+        it 'returns true' do
+          expect(subject).to be true
+        end
+      end
+
+      context 'If the counter of turns is les than 9' do
+        subject { mock_game.full? }
+        it 'returns true' do
+          expect(subject).to be false
+        end
+      end      
     end
 end
