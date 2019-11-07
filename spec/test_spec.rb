64def3df19dff3026bb03b051c694d850c2e4977
David Elì <davidelimg1@gmail.com>
2019-11-07 01:31:17 -0600

    player_index method at game_logic testing implemented

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -16,4 +16,22 @@ describe GameLogic do
         end
       end
     end
+
+    describe '#player_index' do
+      context 'Puts into the slot player selection :X or :O' do
+        it 'Returns the symbol to be fille in the slot' do
+          result = mock_game.player_index(2)
+          expect(result).to eql(:X)
+        end
+      end
+    end
+
+    describe '#board_full?' do
+      context 'If the slot in the board its filled whit X or O' do
+        subject { mock_game.board_full?(1) }
+        it 'returns true' do
+          expect(subject).to be true
+        end
+      end
+    end
 end
